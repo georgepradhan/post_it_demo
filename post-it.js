f04724b06066620e5952ea07a9e8d8548856230c
Apprentice <apprentice@dbc30.local>
2013-09-24 11:48:18 -0700

    add "handle" to draggable method to restrict to header only. split stopPropagation and contenteditable into two event handlers.

--- post-it.js
+++ post-it.js
@@ -10,8 +10,8 @@ var Board = function( selector ) {
 
     $elem.on('click', function(){
       renderPostIt($elem);
-      makeNotesDraggable()
-
+      makeNotesDraggable();
+      makeNotesEditable();
     })
   };
 
@@ -21,31 +21,37 @@ var Board = function( selector ) {
   };
 
   function makeNotesDraggable(){
-    $( ".post-it" ).draggable();    
-  }
+    $('.post-it').draggable({handle: ".header"});
+  };
+
+  function makeNotesEditable(){
+    $( ".post-it" ).on('click', function( event ) {
+      event.stopPropagation();
+    })
+
+    $( ".post-it .content" ).on('click', function() {
+      // alert('hi')
+      $(this).attr("contenteditable", "true")
+    })
+
+  };
 
   initialize();
 };
-
-
 
 
 var PostIt = function() {
   // Your post-it related code goes here
   var post_it_stuff = " class=\"post-it\">"
   var header_stuff = " class=\"header\">"
-  var content_stuff = " class=\"content\" contenteditable=\"true\">"
+  var content_stuff = " class=\"content\">"
 
   this.object = 
   "<div"+post_it_stuff+
     "<div"+header_stuff+"</div>"+
     "<div"+content_stuff+"</div>"+
   "</div>"
-
 };
-
-
-
 
 
 $(function() {
@@ -53,3 +59,20 @@ $(function() {
   // Board.new('#board'); // original code...  
   new Board('#board');
 });
+
+
+// problems: 
+
+// draggable is making it so we can only right-click
+// but we want to be able to left-click and enter the element.
+
+// we want to be able to drag the header alone, but that doesn't carry 
+// the content along with the header: just hte header alone
+
+// we have no exit-button 
+
+// we don't append the new post its to where we click (goes to the top left corner)
+
+
+
+
